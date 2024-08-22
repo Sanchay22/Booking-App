dc4d6e52bd680455e645d2e3eb67fb04c66ad377
Sanchay Jadon <jadonsanchay@gmail.com>
2024-08-22 07:33:41 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose';
+import userRoutes from './routes/users.routes';
+import authRoutes from "./routes/auth.routes"
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(7000,()=>{
+    console.log("Very good")
+})
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom";
+const Header=()=> {
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+            <Link to="/">
+            HoliKing.com</Link>
+        </span>
+
+        <span className='flex space-x-2 bg-white'>
+            <Link to="/sign-in" className='flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-200 hover:text-green-400'>
+            Sign In
+           </Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
