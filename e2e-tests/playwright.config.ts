70dfb611310bd5700edc7b297f24d4efebbe607d
Sanchay Jadon <jadonsanchay@gmail.com>
2024-08-28 23:15:54 +0530

    chore: Update npm dependency to TypeScript 5.5.4

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'; 
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
