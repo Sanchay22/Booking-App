89fdf8960c3b1c75f54262796b4765c868c57943
Sanchay Jadon <jadonsanchay@gmail.com>
2024-09-02 21:26:16 +0530

    chore: Update imports for Hotel model and types

--- backend/src/routes/my-hotels.routes.ts
+++ backend/src/routes/my-hotels.routes.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 import cloudinary from "cloudinary"
-import Hotel, { HotelType } from "../models/hotel.models";
+import Hotel from "../models/hotel.models";
+import { HotelType } from "../shared/types";
 import verifyToken from "../middleware/auth.middleware";
 import { body } from "express-validator";
 const router = express.Router();
