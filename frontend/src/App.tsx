45e90d8d080115dbd5e75d4620469e406e75bd33
Sanchay Jadon <jadonsanchay@gmail.com>
2024-09-02 21:01:44 +0530

    feature:added feature of my-hotels

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,10 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 const App=()=>{
   const {isLoggedIn}=useAppContext();
-  return (<>
+  return (
   <Router>
       <Routes>
         <Route path="/" element={<Layout>
@@ -29,11 +30,17 @@ const App=()=>{
               <AddHotel />
             </Layout>
           } />
+          <Route path="/my-hotels" element={
+            <Layout>
+              <MyHotels />
+            </Layout>
+          } 
+          />
         </>}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  </>);
+    );
 };
 
-export default App
+export default App;
